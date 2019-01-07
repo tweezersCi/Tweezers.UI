7cc7ac5c2afedbfac5575555c728633d7f59066a
Nissan Cohen <nissanc6@gmail.com>
2019-01-07 21:59:33 +0200

    UI: Adding vuetify

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import './plugins/vuetify';
+import vueHeadful from 'vue-headful';
+import App from './App.vue';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.component('vue-headful', vueHeadful);
 
-/* eslint-disable no-new */
+Vue.config.productionTip = false;
+
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App),
+}).$mount('#app');
