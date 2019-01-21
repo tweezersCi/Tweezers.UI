6c158c047adbd6b61ca1021e3f7b7403c302870c
Nissan Cohen <nissanc6@gmail.com>
2019-01-21 20:38:47 +0200

    Vue: Adding components for side menu

---
+++ src/components/main-menu/SideMenuItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-divider :inset="value.inset" :key="index"></v-divider>
+
+    <v-list-tile :key="value.title" avatar ripple @click="cat()">
+      <v-list-tile-avatar color="white">
+        <v-icon color="purple">{{ value.entityData.iconName }}</v-icon>
+      </v-list-tile-avatar>
+
+      <v-list-tile-content>
+        <v-list-tile-title v-html="value.entityData.displayName"></v-list-tile-title>
+        <v-list-tile-sub-title v-html="value.name"></v-list-tile-sub-title>
+      </v-list-tile-content>
+    </v-list-tile>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    index: {
+        type: Number,
+        required: true
+    },
+    value: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
