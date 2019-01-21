f92e248f6805979ae6ad6c60388a8033652ec16f
Nissan Cohen <nissanc6@gmail.com>
2019-01-21 20:39:38 +0200

    Vue: Fixing lint errors

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,11 @@
 
 <script>
 import MainMenu from './components/main-menu/MainMenu.vue';
-import SideMenu from "./components/main-menu/SideMenu.vue";
 
 export default {
   name: 'App',
   components: {
     MainMenu,
-    SideMenu,
   },
   data() {
     return {
