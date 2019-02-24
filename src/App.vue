b9c600c8832b118510981dcf5ddc4e3cba6e7867
Nissan Cohen <nissanc6@gmail.com>
2019-02-24 22:43:02 +0200

    Revert "UI: Main menu init"
    This reverts commit b13e5ab27df656b141ab740310c6ef82c3ec3757.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,33 @@
 <template>
-  <MainMenu/>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import MainMenu from './components/main-menu/MainMenu.vue';
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    MainMenu,
+    HelloWorld,
   },
   data() {
     return {
