fdb1e3b7ddaacea9dbc06d9a3ad676a11f28e264
Nissan Cohen <nissanc6@gmail.com>
2019-02-24 22:43:08 +0200

    Revert "UI: Adding vuetify"
    This reverts commit d527db01e43df6c52099d64a88db4c744c7a0d75.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
-import Vue from 'vue';
-import './plugins/vuetify';
-import vueHeadful from 'vue-headful';
-import App from './App.vue';
-import router from './router';
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
 
-Vue.component('vue-headful', vueHeadful);
+Vue.config.productionTip = false
 
-Vue.config.productionTip = false;
-
+/* eslint-disable no-new */
 new Vue({
+  el: '#app',
   router,
-  render: h => h(App),
-}).$mount('#app');
+  components: { App },
+  template: '<App/>'
+})
