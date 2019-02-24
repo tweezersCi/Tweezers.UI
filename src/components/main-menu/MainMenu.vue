4868152300ca3bfbffd1313e81bba94710f3eb2c
Nissan Cohen <nissanc6@gmail.com>
2019-02-24 22:42:54 +0200

    Revert "Vue: Adding components for side menu"
    This reverts commit 03677aa40fd445c5036d16e64f86e4da2f4d5cb9.

--- src/components/main-menu/MainMenu.vue
+++ src/components/main-menu/MainMenu.vue
@@ -1,10 +1,21 @@
 <template>
   <v-app id="sandbox" :dark="dark">
-    <SideMenu ref="sideMenu"/>
+    <v-navigation-drawer
+      v-model="primaryDrawer.model"
+      :permanent="primaryDrawer.type === 'permanent'"
+      :temporary="primaryDrawer.type === 'temporary'"
+      :clipped="primaryDrawer.clipped"
+      :floating="primaryDrawer.floating"
+      :mini-variant="primaryDrawer.mini"
+      absolute
+      overflow
+      app
+    >
+    </v-navigation-drawer>
     <v-toolbar :clipped-left="primaryDrawer.clipped" app absolute>
       <v-toolbar-side-icon
         v-if="primaryDrawer.type !== 'permanent'"
-        @click.stop="changeSideMenu()"
+        @click.stop="primaryDrawer.model = !primaryDrawer.model"
       ></v-toolbar-side-icon>
       <v-toolbar-title>Tweezers UI</v-toolbar-title>
     </v-toolbar>
@@ -12,6 +23,40 @@
       <v-container fluid>
         <v-layout align-center justify-center>
           <v-flex xs10>
+            <v-card>
+              <v-card-text>
+                <v-layout row wrap>
+                  <v-flex xs12 md6>
+                    <span>Scheme</span>
+                    <v-switch v-model="dark" primary label="Dark"></v-switch>
+                  </v-flex>
+                  <v-flex xs12 md6>
+                    <span>Drawer</span>
+                    <v-radio-group v-model="primaryDrawer.type" column>
+                      <v-radio
+                        v-for="drawer in drawers"
+                        :key="drawer"
+                        :label="drawer"
+                        :value="drawer.toLowerCase()"
+                        primary
+                      ></v-radio>
+                    </v-radio-group>
+                    <v-switch v-model="primaryDrawer.clipped" label="Clipped" primary></v-switch>
+                    <v-switch v-model="primaryDrawer.floating" label="Floating" primary></v-switch>
+                    <v-switch v-model="primaryDrawer.mini" label="Mini" primary></v-switch>
+                  </v-flex>
+                  <v-flex xs12 md6>
+                    <span>Footer</span>
+                    <v-switch v-model="footer.inset" label="Inset" primary></v-switch>
+                  </v-flex>
+                </v-layout>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn flat>Cancel</v-btn>
+                <v-btn flat color="primary">Submit</v-btn>
+              </v-card-actions>
+            </v-card>
           </v-flex>
         </v-layout>
       </v-container>
@@ -23,33 +68,28 @@
 </template>
 
 <script>
-import SideMenu from "./SideMenu.vue";
+const axios = require('axios');
 
 export default {
-  components: {
-    SideMenu,
-  },
   data: () => ({
-    pageInfo: [],
+    info: {},
     dark: true,
     drawers: ['Default (no property)', 'Permanent', 'Temporary'],
     primaryDrawer: {
       model: null,
       type: 'default (no property)',
-      clipped: true,
+      clipped: false,
       floating: false,
       mini: false,
     },
     footer: {
       inset: false,
     },
-    methods: {
-    }
   }),
-  methods: {
-    changeSideMenu() {
-      this.$refs.sideMenu.changeModel();
-    }
-  }
+  mounted() {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then((response) => { this.info = response.data.bpi; });
+  },
 };
 </script>
