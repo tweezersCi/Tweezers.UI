01ba52a86da818fd3ca81e01857bfb2558a300a5
Nissan Cohen <nissanc6@gmail.com>
2019-02-27 21:27:43 +0200

    Styles: Adding the Indigo-Pink custom style

---
+++ src/app/tweezers/modules/single-item-controls/text-box/TextBoxComponent.ts
@@ -0,0 +1,25 @@
+import { Component, Input, SimpleChanges } from "@angular/core";
+import { BaseComponent } from '../../base-component/BaseComponent';
+
+@Component({
+    selector: 'tweezers-text-box',
+    templateUrl: 'text-box.component.html',
+    styleUrls: ['text-box.component.css']
+})
+export class TextBoxComponent extends BaseComponent{
+    @Input() prop: string;
+    @Input() header: string;
+    @Input() item: any;
+    private value:string;
+
+    ngOnInit(): void {
+        this.value = this.item[this.prop];
+    }
+
+    onChange(e) {
+        console.log("e", e);
+        console.log("currentValue", e.target.value);
+
+        this.item[this.prop] = e.target.value
+    }
+}
