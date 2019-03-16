7e0b8b0020777cd48b7b90ba30d38fa253f9ff69
Nissan Cohen <nissanc6@gmail.com>
2019-03-16 20:06:39 +0200

    Single Item: Adding basic new item functionality closes #49

--- src/app/tweezers/utils/tweezers-api.ts
+++ src/app/tweezers/utils/tweezers-api.ts
@@ -17,6 +17,12 @@ export class TweezersApi {
         this.baseUrl = config.url;
     }
 
+    public getBaseLinkKey(refLink: string) {
+        const parts = refLink.split('/').filter(el => el);
+        const refLinkKey = parts.find(el => true);
+        return refLinkKey;
+    }
+
     public async discoverBaseEntities(): Promise<ClassBaseMetadata[]> {
         const discoverUrl = `${this.baseUrl}/api/tweezers`;
         return this.get(discoverUrl).then((res) => {
@@ -39,9 +45,11 @@ export class TweezersApi {
         return this.get(url);
     }
 
-    public async saveEntity(itemUrl: string, item: any): Promise<any> {
-        const url = this.Sanitize(`${this.baseUrl}/api/${itemUrl}`);
-        return this.patch(url, item);
+    public async saveEntity(itemUrl: string, item: any, isNew: boolean = false): Promise<any> {
+        const url = isNew 
+            ? this.Sanitize(`${this.baseUrl}/api/${this.getBaseLinkKey(itemUrl)}`)
+            : this.Sanitize(`${this.baseUrl}/api/${itemUrl}`);
+        return isNew ? this.post(url, item) : this.patch(url, item);
     }
 
     private async get(url: string): Promise<any> {
@@ -56,8 +64,20 @@ export class TweezersApi {
         });
     }
 
-    public async patch(url: string, body: any) {
+    public async patch(url: string, body: any): Promise<any> {
         return this.http.patch(url, body, {headers: this.getHeaders()}).toPromise().then((res) => {
+            if (res) {
+                return res;
+            }
+        }).catch((err) => {
+            // That's a bad error handling, but will do for now.
+            console.log(err);
+            return null;
+        });
+    }
+
+    public async post(url: string, body: any): Promise<any> {
+        return this.http.post(url, body, {headers: this.getHeaders()}).toPromise().then((res) => {
             if (res) {
                 return res;
             }
