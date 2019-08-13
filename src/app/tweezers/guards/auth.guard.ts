6afe81e5b773fc82b4dbaae277265c723dc07164
Nissan Cohen <nissanc6@gmail.com>
2019-08-13 20:15:36 +0300

    Fixing login bugs

--- src/app/tweezers/guards/auth.guard.ts
+++ src/app/tweezers/guards/auth.guard.ts
@@ -13,7 +13,7 @@ export class AuthGuard implements CanActivate {
             return true;
         }
 
-        this.router.navigate(['/login'], {});
+        window.location.href = '/login';
         return false;
     }
 }
